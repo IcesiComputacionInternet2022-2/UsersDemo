e50b2c6b16fc0ce3cf92c096f31c4d1cc1302953
JulianAndresRiveraCarrillo <julianandresrivera29@hotmail.com>
2022-08-25 15:51:03 -0500

    Init project

--- src/main/java/com/icesi/edu/users/controller/UserController.java
+++ src/main/java/com/icesi/edu/users/controller/UserController.java
@@ -3,18 +3,20 @@ package com.icesi.edu.users.controller;
 import com.icesi.edu.users.api.UserAPI;
 import com.icesi.edu.users.dto.UserDTO;
 import com.icesi.edu.users.mapper.UserMapper;
+import com.icesi.edu.users.model.User;
 import com.icesi.edu.users.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 public class UserController implements UserAPI {
-
 
     public final UserService userService;
     public final UserMapper userMapper;
@@ -26,7 +28,32 @@ public class UserController implements UserAPI {
 
     @Override
     public UserDTO createUser(UserDTO userDTO) {
-        return userMapper.fromUser(userService.createUser(userMapper.fromDTO(userDTO)));
+        if (validateEmail(userDTO.getEmail()) && validatePhoneNumber(userDTO.getPhoneNumber())) {
+            return userMapper.fromUser(userService.createUser(userMapper.fromDTO(userDTO)));
+        }else{
+            return null;
+        }
+    }
+
+    private boolean validatePhoneNumber(String phoneNumber) {
+        /*Pattern pattern = Pattern .compile("((\\+[57]{3,4}|0[57]{4}|00[57]{3})\\-?)?\\d{10,20}");
+
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        if(matcher.matches()){
+            return true;
+        }else{
+            throw new RuntimeException("Numero Invalido ");
+        }*/
+    }
+
+    private boolean validateEmail(String email) {
+        String[] temp = email.split("@");
+        if(temp[1].equals("icesi.edu.co") && (temp[0].matches("^[0-9a-zA-z]*$"))){
+            return true;
+        }else{
+            throw new RuntimeException("Correo Invalido");
+        }
     }
 
     @Override
