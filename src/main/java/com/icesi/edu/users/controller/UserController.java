95f2b095931e39b633e491af71ec5764d318ba7b
juanserodriguez16 <juseros9@gmail.com>
2022-08-25 15:48:17 -0500

    Controller

--- src/main/java/com/icesi/edu/users/controller/UserController.java
+++ src/main/java/com/icesi/edu/users/controller/UserController.java
@@ -33,4 +33,35 @@ public class UserController implements UserAPI {
     public List<UserDTO> getUsers() {
         return userService.getUsers().stream().map(userMapper::fromUser).collect(Collectors.toList());
     }
+
+    public boolean emailDomain(String email){
+        boolean domain = false;
+        String[] newStr =  email.split("@");
+        if(newStr[0].matches("[^\\w]") && newStr[1].equals("icesi.edu.co")){
+            domain = true;
+        }
+        return domain;
+    }
+    public boolean phoneValidation(String phone) {
+        boolean isNumber = false;
+        int contador = 0;
+        for (int i = 0; i < phone.length(); i++){
+            if (phone.charAt(i) == ' ') {
+                contador++;
+            }
+        }
+
+        if(phone.startsWith("+57") && (phone.length() == 13) && (contador != 0)){
+            isNumber = true;
+        }
+        return isNumber;
+    }
+    public boolean firstNamelength(String firstName){
+        return(firstName.length() <= 120);
+    }
+    public boolean lastNamelength(String lastName){
+        return(lastName.length() <= 120);
+    }
+
+
 }
