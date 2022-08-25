18f461e7a51839409d5d4d8e6e06668ad0ff826c
nicolaspenagos <nicolas.penagosm98@gmail.com>
2022-08-25 15:46:55 -0500

    Work done during lecture time added

--- src/main/java/com/icesi/edu/users/controller/UserController.java
+++ src/main/java/com/icesi/edu/users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.icesi.edu.users.controller;
 import com.icesi.edu.users.api.UserAPI;
 import com.icesi.edu.users.dto.UserDTO;
 import com.icesi.edu.users.mapper.UserMapper;
+import com.icesi.edu.users.model.User;
 import com.icesi.edu.users.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,11 +27,40 @@ public class UserController implements UserAPI {
 
     @Override
     public UserDTO createUser(UserDTO userDTO) {
-        return userMapper.fromUser(userService.createUser(userMapper.fromDTO(userDTO)));
+
+        if(validateEmail(userDTO.getEmail()) && validatePhoneNumber(userDTO.getPhoneNumber())){
+            return userMapper.fromUser(userService.createUser(userMapper.fromDTO(userDTO)));
+        }
+
+        throw new RuntimeException("Invalid data");
+
     }
 
     @Override
     public List<UserDTO> getUsers() {
         return userService.getUsers().stream().map(userMapper::fromUser).collect(Collectors.toList());
     }
+
+    private boolean validateEmail(String email){
+
+        String[] parts = email.split("@");
+        return parts[0].matches("^[0-9a-zA-Z]+$") && parts[1].equals("icesi.edu.co");
+
+    }
+
+    private boolean validatePhoneNumber(String phoneNumber){
+
+
+        String prefix = phoneNumber.substring(0,3);
+        String number = phoneNumber.substring(3);
+
+        System.out.println(prefix+" "+ number);
+        if(prefix.equals("+57") && number.matches("^[0-9]+$") && number.length()==10){
+          return true;
+        }
+        return true;
+
+    }
+
+
 }
