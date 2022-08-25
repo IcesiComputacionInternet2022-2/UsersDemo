54d0b694b7d5b6287e5f2c5e8370e0d575018555
Juan José Calderón <juancalderon721@outlook.com>
2022-08-25 15:42:15 -0500

    Added almost all the controller validations
    The validation of the special characters of the first and last name still remain to be implemented, besides the connection of all the validations in order to test it

--- src/main/java/com/icesi/edu/users/controller/UserController.java
+++ src/main/java/com/icesi/edu/users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.icesi.edu.users.controller;
 import com.icesi.edu.users.api.UserAPI;
 import com.icesi.edu.users.dto.UserDTO;
 import com.icesi.edu.users.mapper.UserMapper;
+import com.icesi.edu.users.model.User;
 import com.icesi.edu.users.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +34,35 @@ public class UserController implements UserAPI {
     public List<UserDTO> getUsers() {
         return userService.getUsers().stream().map(userMapper::fromUser).collect(Collectors.toList());
     }
+
+    private boolean validateEmailOrPhone(UserDTO userDTO){
+        return userDTO.getEmail() != null && userDTO.getPhoneNumber() != null;
+    }
+
+    private boolean validateEmailDomain(String email){
+        return email !=null && email.matches("@icesi\\.edu\\.co$");
+    }
+
+    private boolean validateEmailUsername(String email){
+        return email.matches("^[a-zA-Z0-9._-]+@");
+    }
+
+    private boolean validatePhoneNumber(String phone){
+        return phone != null && phone.matches("^\\+57[0-9]{10}$");
+    }
+    private boolean validateFirstNameLength(String firstName){
+        return false;
+    }
+
+    private boolean validateLastNameLength(String lastName){
+        return false;
+    }
+
+
+
+
+
+
+
+
 }
---
+++ src/test/java/com/icesi/edu/users/repository/controller/UserControllerTest.java
@@ -0,0 +1,17 @@
+package com.icesi.edu.users.repository.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+
+    @BeforeEach
+    public void init(){
+
+    }
+
+//    @Test
+//    publiv void test
+
+
+}
