80d335dd7a4e7e8a6e7ca05b79d36bee05c05b5b
ALEXJR2002 <alexssjr2002@gmail.com>
2022-08-25 15:48:38 -0500

    email and phone validation implemented

--- src/main/java/com/icesi/edu/users/controller/UserController.java
+++ src/main/java/com/icesi/edu/users/controller/UserController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 public class UserController implements UserAPI {
 
+    private final String domain = "@icei.edu.co";
 
     public final UserService userService;
     public final UserMapper userMapper;
@@ -28,6 +31,25 @@ public class UserController implements UserAPI {
     public UserDTO createUser(UserDTO userDTO) {
         return userMapper.fromUser(userService.createUser(userMapper.fromDTO(userDTO)));
     }
+
+    //Email Validation
+    public boolean belongToDomain (String string) {return string.endsWith(domain);}
+
+    private String getUsernameDomain (String email) {return email.substring(0,email.indexOf(domain));}
+
+    public boolean isValidEmail (String emailUsername) {
+        Pattern pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(emailUsername);
+
+        return matcher.find();
+    }
+
+    //Phone Validation
+    public boolean hasColombianPrefix (String number) {return number.startsWith("+57");}
+
+    public boolean hasWhitespaces (String number) {return number.contains(" ");}
+
+    public boolean isValidNumber (String number) {return number.matches("(\\+573)\\d{10}");}
 
     @Override
     public List<UserDTO> getUsers() {
