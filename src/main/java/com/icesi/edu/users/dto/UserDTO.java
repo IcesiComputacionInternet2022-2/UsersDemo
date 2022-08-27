8b4409494ffc5fab0b6ed0ab8535cd00e5154c73
julian-b24 <julian.camilob24@gmail.com>
2022-08-27 16:22:02 -0500

    Request date added on getUser

--- src/main/java/com/icesi/edu/users/dto/UserDTO.java
+++ src/main/java/com/icesi/edu/users/dto/UserDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Data
@@ -20,4 +22,6 @@ public class UserDTO {
     private String firstName;
 
     private String lastName;
+
+    private String requestDate = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
 }
