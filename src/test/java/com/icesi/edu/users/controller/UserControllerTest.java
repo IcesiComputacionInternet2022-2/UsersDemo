1af23b33334e93c27ac2abfa2f3bc89f38e18bdd
Diego H <d13g01hl@gmail.com>
2022-08-27 23:59:31 -0500

    Defined test methods for controller

---
+++ src/test/java/com/icesi/edu/users/controller/UserControllerTest.java
@@ -0,0 +1,93 @@
+package com.icesi.edu.users.controller;
+
+import com.icesi.edu.users.mapper.UserMapper;
+import com.icesi.edu.users.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class UserControllerTest {
+
+    private UserController userController;
+    private UserService userService;
+    private UserMapper userMapper;
+
+    @BeforeEach
+    private void init() {
+        userService = mock(UserService.class);
+        userMapper = mock(UserMapper.class);
+        userController = new UserController(userService, userMapper);
+    }
+
+    @Test
+    public void testGetNullUser() {
+
+    }
+
+    @Test
+    public void testGetUser() {
+
+    }
+
+    @Test
+    public void testCreateCorrectUser() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongName1() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongName2() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongName3() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongName4() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongEmail1() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongEmail2() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongEmail3() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongNumber1() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongNumber2() {
+
+    }
+
+    @Test
+    public void testCreateUserWithWrongNumber3() {
+
+    }
+
+    @Test
+    public void testGetUsers() {
+
+    }
+
+}
