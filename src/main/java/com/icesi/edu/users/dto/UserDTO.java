84cc23b938b24547039d661eb0436eb01bfaac17
ALEXJR2002 <alexssjr2002@gmail.com>
2022-09-01 22:48:11 -0500

    LocalTime variable now is initialized

--- src/main/java/com/icesi/edu/users/dto/UserDTO.java
+++ src/main/java/com/icesi/edu/users/dto/UserDTO.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
     private UUID id;
@@ -23,4 +22,8 @@ public class UserDTO {
     private String lastName;
 
     private LocalTime localTime;
+
+    public UserDTO() {
+        localTime = LocalTime.now();
+    }
 }
