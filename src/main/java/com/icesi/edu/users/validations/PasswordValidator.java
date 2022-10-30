6079d9e22e80987f16c222d2b7782490234a73e3
BrianR <briomero76@gmail.com>
2022-10-30 11:50:36 -0500

    Regex added

--- src/main/java/com/icesi/edu/users/validations/PasswordValidator.java
+++ src/main/java/com/icesi/edu/users/validations/PasswordValidator.java
@@ -4,9 +4,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<CustomAnnotation.PasswordValidation, String> {
-
+    final String REGEX = "^(?=.*\\d)(?=.*[\\u0021-\\u002b\\u003c-\\u0040])(?=.*[A-Z])(?=.*[a-z])\\S{4,}$";
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return true;
+
+        return s.matches(REGEX);
     }
 }
