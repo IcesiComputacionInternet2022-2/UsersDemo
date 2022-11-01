7c45e45c446cc189dc42ba50c406ec7bac62f9da
a <b>
2022-11-01 16:11:05 -0500

    Server date and time added

--- src/main/java/com/icesi/edu/users/controller/ViewController.java
+++ src/main/java/com/icesi/edu/users/controller/ViewController.java
@@ -5,12 +5,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import java.time.LocalDateTime;
+
+
 @Controller
 @AllArgsConstructor
 public class ViewController implements ViewAPI {
     @Override
     public String getHomePage(Model model) {
         model.addAttribute("welcome", "Hola");
+        model.addAttribute("serverTime", LocalDateTime.now());
         return "index";
     }
 }
