fa5f4ad516da0f6462248037ec72a4273acca2a2
juseprada96 <juseprada96@gmail.com>
2022-11-03 13:14:11 -0500

    thymeleaf

---
+++ src/main/java/com/icesi/edu/users/config/MessageSourceConfig.java
@@ -0,0 +1,16 @@
+package com.icesi.edu.users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
